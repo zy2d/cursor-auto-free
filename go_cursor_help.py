c425f548cf650763d597a90364b6c920f78e86f5
gorlev <leventgoral95@gmail.com>
2025-03-16 23:49:20 +0300

    English language support added

--- go_cursor_help.py
+++ go_cursor_help.py
@@ -2,28 +2,29 @@ import platform
 import os
 import subprocess
 from logger import logging
+from language import get_translation
 
 def go_cursor_help():
     system = platform.system()
-    logging.info(f"当前操作系统: {system}")
+    logging.info(get_translation("current_operating_system", system=system))
     
     base_url = "https://aizaozao.com/accelerate.php/https://raw.githubusercontent.com/yuaotian/go-cursor-help/refs/heads/master/scripts/run"
     
     if system == "Darwin":  # macOS
         cmd = f'curl -fsSL {base_url}/cursor_mac_id_modifier.sh | sudo bash'
-        logging.info("执行macOS命令")
+        logging.info(get_translation("executing_macos_command"))
         os.system(cmd)
     elif system == "Linux":
         cmd = f'curl -fsSL {base_url}/cursor_linux_id_modifier.sh | sudo bash'
-        logging.info("执行Linux命令")
+        logging.info(get_translation("executing_linux_command"))
         os.system(cmd)
     elif system == "Windows":
         cmd = f'irm {base_url}/cursor_win_id_modifier.ps1 | iex'
-        logging.info("执行Windows命令")
-        # 在Windows上使用PowerShell执行命令
+        logging.info(get_translation("executing_windows_command"))
+        # Use PowerShell to execute command on Windows
         subprocess.run(["powershell", "-Command", cmd], shell=True)
     else:
-        logging.error(f"不支持的操作系统: {system}")
+        logging.error(get_translation("unsupported_operating_system", system=system))
         return False
     
     return True
